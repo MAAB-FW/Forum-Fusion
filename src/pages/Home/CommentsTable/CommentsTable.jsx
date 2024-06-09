91e1eb4963957f3409fabd3f7bfe5df7b081491e
maab-fw <maabfw@proton.me>
2024-06-09 20:16:10 +0600

    pagination implimented on commentsTable page

--- src/pages/Home/CommentsTable/CommentsTable.jsx
+++ src/pages/Home/CommentsTable/CommentsTable.jsx
@@ -1,29 +1,42 @@
-import React from "react"
+import React, { useState } from "react"
 import { Table, TableBody, TableHead, TableHeader, TableRow } from "@/components/ui/table"
 import useAxiosSecure from "@/hooks/useAxiosSecure"
 import { useQuery } from "@tanstack/react-query"
 import { useParams } from "react-router-dom"
 import CommentRow from "./CommentRow"
 import SmallLoading from "@/components/SmallLoading"
+import Pagination from "@/components/Pagination"
 
 const CommentsTable = () => {
     const { id } = useParams()
-
     const axiosSecure = useAxiosSecure()
+    const [currentPage, setCurrentPage] = useState(0)
+    const itemPerPage = 5
+    const { data, isFetching: isFetching2 } = useQuery({
+        queryKey: ["commentsCount"],
+        queryFn: async () => {
+            const res = await axiosSecure(`/commentsCount/${id}`)
+            // console.log(res.data)
+            return res.data
+        },
+        initialData: {},
+    })
+    const { count } = data
+
     const {
         data: comments,
         refetch,
         isFetching,
     } = useQuery({
-        queryKey: ["comments"],
+        queryKey: ["comments", currentPage],
         queryFn: async () => {
-            const res = await axiosSecure(`/comments/${id}`)
+            const res = await axiosSecure(`/comments/${id}?size=${itemPerPage}&page=${currentPage}`)
             return res.data
         },
         initialData: [],
     })
 
-    if (isFetching) {
+    if (isFetching || isFetching2) {
         return <SmallLoading />
     }
 
@@ -47,6 +60,12 @@ const CommentsTable = () => {
                         ))}
                 </TableBody>
             </Table>
+            <Pagination
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+                count={count}
+                itemPerPage={itemPerPage}
+            ></Pagination>
         </div>
     )
 }
